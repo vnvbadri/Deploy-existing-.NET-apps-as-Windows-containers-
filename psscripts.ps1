cdcffc4f0f5b57296085b4eaacbc8e1061ea1e39
Vamshi Krishna Badri <vbadri@venhan.com>
2021-01-03 21:36:29 +0530

    lk

--- psscripts.ps1
+++ psscripts.ps1
@@ -1,9 +1,5 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\CustomscriptLogs.txt -Append
 
-Add-LocalGroupMember -Group "docker-users" -Member "$env:UserName"
-
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
-
 function enable-copypagecontent-in-internetexplorer{
 Set-ItemProperty -Path $HKLM -Name "1407" -Value 0
 Set-ItemProperty -Path $HKCU -Name "1407" -Value 0 
