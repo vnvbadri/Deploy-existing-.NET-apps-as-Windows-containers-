dac6f58b6a755edd98b339bbbcee8faf76bc7bf6
Vamshi Krishna Badri <vbadri@venhan.com>
2021-01-03 21:03:44 +0530

    Update psscripts.ps1

--- psscripts.ps1
+++ psscripts.ps1
@@ -1,6 +1,5 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\CustomscriptLogs.txt -Append
 
-
 Add-LocalGroupMember -Group "docker-users" -Member "$env:UserName"
 
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
