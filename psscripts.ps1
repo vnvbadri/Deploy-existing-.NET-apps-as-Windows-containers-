9a2ca313e577b96319d25d9cd5c2729ba4d77828
Vamshi Krishna Badri <vbadri@venhan.com>
2021-01-05 21:38:28 +0530

    Update psscripts.ps1

--- psscripts.ps1
+++ psscripts.ps1
@@ -27,7 +27,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Add-LocalGroupMember -Group "d
 
 # Enable ASPNETMVC4
 Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/9b3476ff-6d0a-4ff8-956d-270147f21cd4/76e39c746d9e2fc3eadd003b5b11440bcf926f3948fb2df14d5938a1a8b2b32f/vs_Community.exe" -OutFile "C:\vs_Community.exe"
-C:\vs_community.exe -q --wait --add  Microsoft.VisualStudio.Web.Mvc4.ComponentGroup
+C:\vs_community.exe -q --add  Microsoft.VisualStudio.Web.Mvc4.ComponentGroup
 
 #Enable Hyper-V
 Set-ExecutionPolicy Bypass -Scope Process -Force; Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All -NoRestart
