0e8a78f3bf5a10b77af880d5e2af4110582d10c5
Vamshi Krishna Badri <vbadri@venhan.com>
2021-01-08 21:26:20 +0530

    Update psscripts.ps1

--- psscripts.ps1
+++ psscripts.ps1
@@ -20,13 +20,6 @@ mkdir C:\windows\system32\config\systemprofile\AppData\Local\Temp
 # Docker User Permission
 Set-ExecutionPolicy Bypass -Scope Process -Force; Add-LocalGroupMember -Group "docker-users" -Member "demouser"
 
-# Windows subsystem for linux version change
-#wsl --set-default-version 2
-
-# Enable ASPNETMVC4
-Invoke-WebRequest -Uri "https://download.visualstudio.microsoft.com/download/pr/9b3476ff-6d0a-4ff8-956d-270147f21cd4/76e39c746d9e2fc3eadd003b5b11440bcf926f3948fb2df14d5938a1a8b2b32f/vs_Community.exe" -OutFile "C:\vs_Community.exe"
-C:\vs_community.exe -q --add  Microsoft.VisualStudio.Web.Mvc4.ComponentGroup
-
 #Enable Hyper-V
 Set-ExecutionPolicy Bypass -Scope Process -Force; Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All -NoRestart
 
