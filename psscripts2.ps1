c0302a2f59969d698fc7bd7ebb8ec4c09a3d9174
Vamshi Krishna Badri <vbadri@venhan.com>
2021-01-11 13:19:23 +0530

    Create psscripts2.ps1

---
+++ psscripts2.ps1
@@ -0,0 +1,26 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\CustomscriptLogs.txt -Append
+
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'docker-desktop',`
+            'visualstudio2019community',`
+            'aspnetmvc4.install',`
+            'visualstudio2019-workload-netweb'
+
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+mkdir C:\windows\system32\config\systemprofile\AppData\Local\Temp
+
+# Docker User Permission
+Set-ExecutionPolicy Bypass -Scope Process -Force; Add-LocalGroupMember -Group "docker-users" -Member "demouser"
+
+#Enable Hyper-V
+Set-ExecutionPolicy Bypass -Scope Process -Force; Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All -NoRestart
+
+Restart-Computer
